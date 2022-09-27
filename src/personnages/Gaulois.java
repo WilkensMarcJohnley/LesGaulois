8981a2e8a074ac507a8fa14f4f86937fe3d466d4
joseph wilkens <jsw3430a@ens.fsi>
2022-09-27 19:18:41 +0200

    Create Gaulois.java

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Gaulois {
+	
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	private String nom;
+	private int force;
+	private int effetPotion=1;
+}
+
+// TODO auto-generated method stub
+
+
