860b525a8ce385a441bff7b6cc243ecf4de76bfe
joseph wilkens <jsw3430a@ens.fsi>
2022-09-27 19:58:11 +0200

    fin TP1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,17 +1,112 @@
 package personnages;
 
 public class Gaulois {
-	
-	public Gaulois(String nom, int force) {
-		super();
-		this.nom = nom;
-		this.force = force;
-	}
-	private String nom;
-	private int force;
-	private int effetPotion=1;
+private String nom;
+private int force;
+private int effetPotion = 1;
+public Gaulois(String nom, int force) {
+this.nom = nom;
+this.force = force;
+}
+public String getNom() {
+return nom;
+}
+public void parler(String texte) {
+System.out.println(prendreParole() + "« " + texte + "»");
+}
+private String prendreParole() {
+return "Le gaulois " + nom + " : ";
+}
+public void frapper(Romain romain) {
+System.out.println(nom + " envoie un grand coup dans la mâchoire de "
++ romain.getNom());
+romain.recevoirCoup(force / 3);
+}
+@Override
+public String toString() {
+return "Gaulois [nom=" + nom + ", force=" + force
++ ", effetPotion=" + effetPotion + "]";
+}
+public static void main(String[] args) {
+//TODO créer un main permettant de tester la classe Gaulois
+}
 }
 
-// TODO auto-generated method stub
+public class Romain {
+private String nom;
+private int force;
+public Romain(String nom, int force) {
+this.nom = nom;
+this.force = force;
+}
+public String getNom() {
+return nom;
+}
+public void parler(String texte) {
+System.out.println(prendreParole() + "« " + texte + "»");
+}
+private String prendreParole() {
+return "Le romain " + nom + " : ";
+}
+public void recevoirCoup(int forceCoup) {
+force -= forceCoup;
+if (force > 0) {
+parler("Aïe");
+} else {
+parler("J'abandonne...");
+}
+}
+}
+public class Druide {
+private String nom;
+private int effetPotionMin;
+private int effetPotionMax;
+public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+this.nom = nom;
+this.effetPotionMin = effetPotionMin;
+this.effetPotionMax = effetPotionMax;
+parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller
+d'une force " + effetPotionMin + " à "
++ effetPotionMax + ".");
+}
+public String getNom() {
+return nom;
+}
+public void parler(String texte) {
+System.out.println(prendreParole() + "« " + texte + "»");
+}
+private String prendreParole() {
+return "Le druide " + nom + " : ";
+}
+}
+
+public class Chef {
+private String nom;
+private int force;
+private int effetPotion = 1;
+private Village village;
+public Chef(String nom, int force, int effetPotion, Village village) {
+this.nom = nom;
+this.force = force;
+this.effetPotion = effetPotion;
+this.village = village;
+}
+public String getNom() {
+return nom;
+}
+public void parler(String texte) {
+System.out.println(prendreParole() + "« " + texte + "»");
+}
+private String prendreParole() {
+return "Le chef " + nom + " du village " + village.getNom() + " : ";
+}
+public void frapper(Romain romain) {
+System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
+romain.getNom());
+romain.recevoirCoup(force / 3);
+}
+}
+
+
 
 
