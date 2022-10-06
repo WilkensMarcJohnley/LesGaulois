1719cd88ee84935016e9d71c7ce7ffd74e2dddb2
joseph wilkens <jsw3430a@ens.fsi>
2022-10-06 15:30:08 +0200

    tp2 a finir

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -6,24 +6,24 @@ public class Chef {
 	private int effetPotion=1;
 	private Village village; 
 	
-	public Chef(String nom, int force, int effetPotion, Village village) {
+	public Chef(String nom, int force, Village village) {
 		super();
 		this.nom = nom;
 		this.force = force;
-		this.effetPotion= effetPotion;
 		this.village = village; 
 	}
 	public String getNom() {
 		return nom;
 	}
 	
+
 	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<< " + texte + ">>");
 	}
 	
 	private String prendreParole() {
-		return "Le Chef " + nom + "du village" + village.getnom() + " : ";
+		return "Le Chef " + nom + "du village" + village.getNom() + " : "; 
 	}
 	
 	public void frapper(Romain romain) {
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,11 +3,25 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois=0;
 	
-	public Village(String nom, Chef chef) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
-		this.chef = chef;
+		villageois= new Gaulois[nbVillageoisMaximum];
+		
 	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois]= gaulois;
+		nbVillageois+=1;
+	}
+	
+	public Gaulois trouverHabitant(int numero) {
+		return villageois[numero]; 
+	}
+	
+	
 	
 	public String getNom() {
 		return nom;
@@ -15,6 +29,19 @@ public class Village {
 	
 	public void setChef(Chef chef) {
 		this.chef = chef;
+	}
+	
+	public static void main(String[] args) {
+		Chef abraracourix;
+		Gaulois asterix;
+		Village village;
+		village= new Village("Vilage des Irreductibles",30);
+		//Gaulois gaulois= village.trouverHabitant(30); il faut creer village dans chef et chef dans village en laissant vide le constructeur
+		abraracourix= new Chef("Abraracourix",6,village);
+		asterix= new Gaulois("Asterix", 8);
+		village.ajouterHabitant(asterix);
+		Gaulois gaulois= village.trouverHabitant(0);
+		System.out.println(gaulois); 
 	}
 	
 }
