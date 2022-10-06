16fc3f89a270dc882492cd17c156a46c5db30916
WilkensMarcJohnley <josephwilkensmarcjohnley@outlook.com>
2022-10-06 16:45:35 +0200

    le village gaulois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -12,6 +12,9 @@ public class Village {
 		
 	}
 	
+
+
+
 	public void ajouterHabitant(Gaulois gaulois) {
 		villageois[nbVillageois]= gaulois;
 		nbVillageois+=1;
@@ -21,7 +24,12 @@ public class Village {
 		return villageois[numero]; 
 	}
 	
-	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du Chef " + chef.getNom() + " vivent les legendaires gaulois :");
+		for (int i = 0; i < nbVillageois; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
 	
 	public String getNom() {
 		return nom;
@@ -33,15 +41,20 @@ public class Village {
 	
 	public static void main(String[] args) {
 		Chef abraracourix;
-		Gaulois asterix;
+		Gaulois asterix,obelix;
 		Village village;
 		village= new Village("Vilage des Irreductibles",30);
 		//Gaulois gaulois= village.trouverHabitant(30); il faut creer village dans chef et chef dans village en laissant vide le constructeur
 		abraracourix= new Chef("Abraracourix",6,village);
+		village.setChef(abraracourix);
 		asterix= new Gaulois("Asterix", 8);
 		village.ajouterHabitant(asterix);
 		Gaulois gaulois= village.trouverHabitant(0);
 		System.out.println(gaulois); 
+		obelix= new Gaulois("Obelix",7);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
+		
 	}
 	
 }
